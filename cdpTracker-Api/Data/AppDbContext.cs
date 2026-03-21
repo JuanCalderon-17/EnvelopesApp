6e7a8b24fa91dbf66740accb4f398c446e2237ca
Juan <juancalderonmicro@hotmail.com>
2026-03-20 23:54:30 -0500

    the first commit, two new project

---
+++ cdpTracker-Api/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace cdpTracker_Api.Data
+{
+    public class AppDbContext
+    {
+        //class contructor,it takes in the options and passes it to the base class constructor
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // this are the tables in the database, each DbSet represents a table in the database and the type parameter is the model class that represents the table
+        public DbSet<Workers> Workers { get; set; }
+        public DbSet<Envelopes> Envelopes { get; set; }
+
+    }
