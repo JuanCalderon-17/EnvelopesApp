eeee1951e5b01691dbcbb9f371a55044c24ec01a
Juan <juancalderonmicro@hotmail.com>
2026-03-26 23:47:14 -0500

    seed for data, this ensure that every copy or download of this code in another device will have the same starting point

--- cdpTracker-Api/Data/AppDbContext.cs
+++ cdpTracker-Api/Data/AppDbContext.cs
@@ -15,5 +15,31 @@ namespace cdpTracker_Api.Data
         // this are the tables in the database, each DbSet represents a table in the database and the type parameter is the model class that represents the table
         public DbSet<Worker> Workers { get; set; }
         public DbSet<Envelope> Envelopes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //seed a default worker to the database, this is useful for testing and development purposes
+            modelBuilder.Entity<Worker>().HasData(new Worker
+            {
+                Id = 1,
+                Name = "Admin Manager",
+                PasswordHash = "hashed_admin_pass",
+                Kiosko = KioskLocation.K2,
+                Role = UserRole.Manager
+            });
+
+
+            //seed a default worker to the database, this is useful for testing and development purposes
+            modelBuilder.Entity<Worker>().HasData(new Worker
+            {
+                    Id = 2,
+                    Name = "Juan Calderon",
+                    PasswordHash = "hashed_john_pass",
+                    Kiosko = KioskLocation.K3, 
+                    Role = UserRole.Worker
+            });
+        }
     }
 }
