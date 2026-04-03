dd5c0f7b4acfd3b972b7290670aa4bff112f48ae
Juan <juancalderonmicro@hotmail.com>
2026-04-02 23:45:24 -0500

    jwt configuration in program.cs file auth and validation

--- cdpTracker-Api/Program.cs
+++ cdpTracker-Api/Program.cs
@@ -1,18 +1,44 @@
-using Microsoft.EntityFrameworkCore;    
 using cdpTracker_Api.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;    
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //connection string for database
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-//
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
 
 // Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+//jwt authentication configuration
+var jwtSettings = builder.Configuration.GetSection("Jwt");  
+var key = Encoding.ASCII.GetBytes(jwtSettings["Key"]);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+
 
 var app = builder.Build();
 
