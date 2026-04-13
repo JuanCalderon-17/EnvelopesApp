1988db2b0e4b4fc0e2556706381e0a2d860f8024
Juan <juancalderonmicro@hotmail.com>
2026-04-12 22:51:50 -0500

    commit to test cors and token validation with angular, first steps

--- cdpTracker-Frontend/src/app/app.component.ts
+++ cdpTracker-Frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EnvelopeService } from './services/envelope.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'cdpTracker-Frontend';
+export class AppComponent implements OnInit{
+  constructor(private envelopeservice: EnvelopeService) {}
+  ngOnInit() {
+    this.envelopeservice.getEnvelopeByWorker(1).subscribe({
+      next: (data) => {
+        console.log('✅ ¡CORS funcionando! Datos recibidos:', data);
+      },
+      error: (err) => {
+        console.error('❌ Error de conexión o CORS:', err);
+      }
+    });
+  }
 }
