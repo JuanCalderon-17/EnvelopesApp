f256236f495c43af685b404904d370ad39f7430e
Juan <juancalderonmicro@hotmail.com>
2026-04-12 22:42:55 -0500

    upload envelope service creation to connect back with front

--- cdpTracker-Frontend/src/app/services/envelope.service.ts
+++ cdpTracker-Frontend/src/app/services/envelope.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnvelopeService {
 
-  constructor() { }
+  //change port to the one used in for the backend 
+  private apiUrl = 'https://localhost:7123/api/envelopes';
+
+  constructor( private http: HttpClient ) { }
+
+  //method for testing conexion and cors 
+  getEnvelopeByWorker( workerId: number): Observable<any> {
+    //token for header in any request, because my app use [authorize]
+    const token = ''
+    
+    const headers =  new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    return this.http.get(`${this.apiUrl}/worker/${workerId}`, { headers });  }
 }
