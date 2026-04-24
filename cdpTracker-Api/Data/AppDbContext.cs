e126a51c6fd74c8e8f45f6c82e5934bdaecd9a7e
Juan <juancalderonmicro@hotmail.com>
2026-04-24 14:34:58 -0500

    Rename kiosks: remove spaces from usernames, fix Neon seed

--- cdpTracker-Api/Data/AppDbContext.cs
+++ cdpTracker-Api/Data/AppDbContext.cs
@@ -21,7 +21,7 @@ namespace cdpTracker_Api.Data
                 new Worker
                 {
                     Id = 1,
-                    Name = "Kiosko 2",
+                    Name = "Kiosko2",
                     PasswordHash = "$2a$11$iE/mpYk0zTDDKPDtfhxPyOZ2lFfxnlAGP/sn0p.yR3PQZtG7e3OS2",
                     Kiosko = KioskLocation.K2,
                     Role = UserRole.Worker
@@ -29,7 +29,7 @@ namespace cdpTracker_Api.Data
                 new Worker
                 {
                     Id = 2,
-                    Name = "Kiosko 3",
+                    Name = "Kiosko3",
                     PasswordHash = "$2a$11$Qhy6d5hyPmjk5kIKhJ8kSek.0wObNSSsHzE6B83DFnnNW6R1cN9sm",
                     Kiosko = KioskLocation.K3,
                     Role = UserRole.Worker
@@ -37,7 +37,7 @@ namespace cdpTracker_Api.Data
                 new Worker
                 {
                     Id = 3,
-                    Name = "Kiosko 5",
+                    Name = "Kiosko5",
                     PasswordHash = "$2a$11$i8X0D4..nZMXolIfRO9RZOS9NZUclVLIZydpOjlej.S7D2m5fUnU.",
                     Kiosko = KioskLocation.K5,
                     Role = UserRole.Worker
---
+++ cdpTracker-Api/Migrations/20260424193410_RenameKiosksNoSpaces.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cdpTracker_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameKiosksNoSpaces : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Workers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Kiosko2");
+
+            migrationBuilder.UpdateData(
+                table: "Workers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Kiosko3");
+
+            migrationBuilder.UpdateData(
+                table: "Workers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Kiosko5");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Workers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Kiosko 2");
+
+            migrationBuilder.UpdateData(
+                table: "Workers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Kiosko 3");
+
+            migrationBuilder.UpdateData(
+                table: "Workers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Kiosko 5");
+        }
+    }
+}
